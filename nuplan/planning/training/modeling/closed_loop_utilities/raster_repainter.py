0a5eb00c6b66a289614cf4ca263381d01b0113ae
jingyu.qian <jingyu.qian@horizon.ai>
2023-01-03 09:41:38 +0800

    Format adjustments, no real code change

--- nuplan/planning/training/modeling/closed_loop_utilities/raster_repainter.py
+++ nuplan/planning/training/modeling/closed_loop_utilities/raster_repainter.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn.functional as F
 from kornia.geometry.transform import get_rotation_matrix2d, warp_affine
 
-from nuplan.planning.training.modeling.closed_loop_utilities.utils import \
+from nuplan.planning.training.modeling.closed_loop_utilities.utils.torch_util import \
     principal_value
 
 DISTANCE_THRESH = 1.5
@@ -14,19 +14,19 @@ ANGLE_THRESH = 30 * math.pi / 180
 def repaint_raster(
     raster: torch.Tensor,
     pose_mats: torch.Tensor,
-    target_pixel_size: float=0.5,
-    dist_thresh: float=DISTANCE_THRESH,
-    angle_thresh: float=ANGLE_THRESH
+    target_pixel_size: float = 0.5,
+    dist_thresh: float = DISTANCE_THRESH,
+    angle_thresh: float = ANGLE_THRESH,
 ):
-    assert raster.shape[-1] == raster.shape[-2], "Requires raster to be square."
+    assert (
+        raster.shape[-1] == raster.shape[-2]
+    ), "Requires raster to be square."
     n = pose_mats.shape[0]
     output_size = raster.shape[-1]
     dist = torch.hypot(pose_mats[:, 0, 2], pose_mats[:, 1, 2])
-    is_dist_in_bound = (dist <= dist_thresh)[:, None, None] 
+    is_dist_in_bound = (dist <= dist_thresh)[:, None, None]
     rot = torch.abs(
-        -principal_value(
-            torch.atan2(pose_mats[:, 1, 0], pose_mats[:, 0, 0])
-        )
+        -principal_value(torch.atan2(pose_mats[:, 1, 0], pose_mats[:, 0, 0]))
     )
     is_rot_in_bound = (rot <= angle_thresh)[:, None, None]
     is_in_bound = torch.logical_and(is_dist_in_bound, is_rot_in_bound)
@@ -36,15 +36,20 @@ def repaint_raster(
     center = (torch.ones(n, 2) * output_size * 0.5).type_as(raster)
     rot_deg = (
         -principal_value(
-            torch.atan2(bounded_pose_mats[:, 1, 0],
-            bounded_pose_mats[:, 0, 0])
-        ) * 180. / math.pi
+            torch.atan2(bounded_pose_mats[:, 1, 0], bounded_pose_mats[:, 0, 0])
+        )
+        * 180.0
+        / math.pi
     ).type_as(raster)
     scale = torch.ones(n, 2).type_as(raster)
 
     affine_mat = get_rotation_matrix2d(center, rot_deg, scale)
-    affine_mat[:, 0, 2] = affine_mat[:, 0, 2] + bounded_pose_mats[:, 1, 2] / target_pixel_size
-    affine_mat[:, 1, 2] = affine_mat[:, 1, 2] + bounded_pose_mats[:, 0, 2] / target_pixel_size
+    affine_mat[:, 0, 2] = (
+        affine_mat[:, 0, 2] + bounded_pose_mats[:, 1, 2] / target_pixel_size
+    )
+    affine_mat[:, 1, 2] = (
+        affine_mat[:, 1, 2] + bounded_pose_mats[:, 0, 2] / target_pixel_size
+    )
 
     output = warp_affine(raster, affine_mat, [output_size, output_size])
     output[:, 0, ...] = raster[:, 0, ...]
--- nuplan/planning/training/modeling/closed_loop_utilities/target_trajectory_recomputer.py
+++ nuplan/planning/training/modeling/closed_loop_utilities/target_trajectory_recomputer.py
@@ -1,12 +1,20 @@
 import numpy as np
 import torch
 
-from nuplan.planning.training.data_augmentation.data_augmentation_util import \
-    ConstrainedNonlinearSmoother
-from nuplan.planning.training.modeling.closed_loop_utilities.utils import (
-    get_trajectories_from_matrix, np_matrix_from_pose, principal_value)
-from nuplan.planning.training.preprocessing.features.abstract_model_feature import \
-    FeatureDataType
+from nuplan.planning.training.data_augmentation.data_augmentation_util import (
+    ConstrainedNonlinearSmoother,
+)
+from nuplan.planning.training.modeling.closed_loop_utilities.utils.torch_util import (
+    principal_value,
+)
+from nuplan.planning.training.modeling.closed_loop_utilities.utils.numpy_util import (
+    get_trajectories_from_matrix,
+    np_matrix_from_pose,
+)
+from nuplan.planning.training.preprocessing.features.abstract_model_feature import (
+    FeatureDataType,
+)
+
 
 def _get_heading(sin_value: FeatureDataType, cos_value: FeatureDataType):
     """Get heading angle in radians.
@@ -22,45 +30,46 @@ def _get_heading(sin_value: FeatureDataType, cos_value: FeatureDataType):
         return principal_value(torch.atan2(sin_value, cos_value))
     else:
         return np.arctan2(sin_value, cos_value)
+
+
 class TargetTrajectoryRecomputer(object):
     """Recomputes trajectories based on perturbed location and a ref location."""
+
     def __init__(
         self,
         trajectory_len: int,
         time_interval: float,
     ):
         self._smoother = ConstrainedNonlinearSmoother(
-            trajectory_len=trajectory_len,
-            dt=time_interval
+            trajectory_len=trajectory_len, dt=time_interval
         )
         self._trajectory_len = trajectory_len
         self._time_interval = time_interval
 
     def reset(self):
         self._smoother = ConstrainedNonlinearSmoother(
-            trajectory_len=self._trajectory_len,
-            dt=self._time_interval
+            trajectory_len=self._trajectory_len, dt=self._time_interval
         )
+
     def recompute(
         self,
         original_target: FeatureDataType,
         pose_matrix: FeatureDataType,
-        speed_magnitude: float
+        speed_magnitude: float,
     ):
-        ref_traj = [[0., 0., 0.]] + original_target.tolist()
+        ref_traj = [[0.0, 0.0, 0.0]] + original_target.tolist()
         x = pose_matrix[0, 2].item()
         y = pose_matrix[1, 2].item()
         h = _get_heading(pose_matrix[1, 0], pose_matrix[0, 0]).item()
         x_curr = [x, y, h, speed_magnitude]
         self._smoother.set_reference_trajectory(
-            x_curr=x_curr,
-            reference_trajectory=ref_traj
+            x_curr=x_curr, reference_trajectory=ref_traj
         )
         try:
             sol = self._smoother.solve()
         except:
             return False, original_target
-        if not sol.stats()['success']:
+        if not sol.stats()["success"]:
             return False, original_target
         else:
             ego_perturb = np.vstack(
@@ -72,7 +81,9 @@ class TargetTrajectoryRecomputer(object):
             )
             ego_perturb = ego_perturb.T[1:]
             ego_perturb = np_matrix_from_pose(ego_perturb)
-            ego_perturb = np.linalg.inv(pose_matrix.cpu().numpy()) @ ego_perturb
+            ego_perturb = (
+                np.linalg.inv(pose_matrix.cpu().numpy()) @ ego_perturb
+            )
             ego_perturb = get_trajectories_from_matrix(ego_perturb)
             new_target = torch.from_numpy(ego_perturb).type_as(original_target)
             return True, new_target
