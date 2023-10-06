ab7b9454a3858f4cca44415821fdeb8b15495637
wenxin.shao <wenxin.shao@horizon.ai>
2023-10-07 04:51:12 +0800

    add kaiming init

--- nuplan/planning/script/builders/model_builder.py
+++ nuplan/planning/script/builders/model_builder.py
@@ -5,8 +5,24 @@ from omegaconf import DictConfig
 
 from nuplan.planning.script.builders.utils.utils_type import validate_type
 from nuplan.planning.training.modeling.torch_module_wrapper import TorchModuleWrapper
+import torch.nn as nn
+import torch.nn.init as init
 
 logger = logging.getLogger(__name__)
+
+
+def kaiming_normal_init(m):
+    # For Convolutional layers
+    if isinstance(m, (nn.Conv1d, nn.Conv2d, nn.Conv3d)):
+        init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+        if m.bias is not None:
+            init.constant_(m.bias, 0)
+    
+    # For Linear layers
+    elif isinstance(m, nn.Linear):
+        init.kaiming_normal_(m.weight, mode='fan_in', nonlinearity='relu')
+        if m.bias is not None:
+            init.constant_(m.bias, 0)
 
 
 def build_torch_module_wrapper(cfg: DictConfig) -> TorchModuleWrapper:
@@ -17,6 +33,7 @@ def build_torch_module_wrapper(cfg: DictConfig) -> TorchModuleWrapper:
     """
     logger.info('Building TorchModuleWrapper...')
     model = instantiate(cfg)
+    # model.apply(kaiming_normal_init)
     validate_type(model, TorchModuleWrapper)
     logger.info('Building TorchModuleWrapper...DONE!')
 
